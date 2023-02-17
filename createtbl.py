725be115c1728604d8807b62804e588dc58c5086
JESUS CORDOVA <cordovangohgster@gmail.com>
2023-02-17 14:19:13 -0500

    lastcommit

---
+++ createtbl.py
@@ -0,0 +1,37 @@
+# Python implementation to create a table in MySQL
+import mysql.connector
+
+# connecting to the mysql server
+
+db = mysql.connector.connect(
+	host="localhost",
+	user="root",
+	passwd="",
+	database="simplepythoncrud"
+)
+
+# cursor object c
+c = db.cursor()
+
+# create statement for tblemployee
+employeetbl_create = """CREATE TABLE `simplepythoncrud`.`tblemployee` (
+`empid` INT NOT NULL AUTO_INCREMENT,
+`empname` VARCHAR(45) NULL,
+`department` VARCHAR(45) NULL,
+`salary` INT NULL,
+PRIMARY KEY (`empid`))"""
+
+c.execute(employeetbl_create)
+
+c = db.cursor()
+
+# fetch tblemployee details in the database
+c.execute("desc tblemployee")
+
+# print the table details
+for i in c:
+	print(i)
+
+
+# finally closing the database connection
+db.close()
